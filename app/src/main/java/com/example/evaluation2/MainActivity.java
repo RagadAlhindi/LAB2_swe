6888c0ff43dc04a3df79c9e994e8b89aa6934550
RagadAlhindi <ragadwh17@gmail.com>
2023-03-29 13:30:47 +0300

    first commit

---
+++ app/src/main/java/com/example/evaluation2/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.evaluation2;
+
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button b ;
+    EditText e;
+
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        b = (Button) findViewById(R.id.button3);
+        e = (EditText) findViewById(R.id.PersonName);
+        b.setOnClickListener (new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String s = e.getText().toString();
+                Intent intent = new Intent(getApplicationContext(), SecondActivity.class);
+                intent.putExtra("personName", s);
+                startActivity(intent);
+            }  });
+    }
+
+
+
+
+
+
+
+}
+
---
+++ app/src/main/java/com/example/evaluation2/SecondActivity.java
@@ -0,0 +1,25 @@
+package com.example.evaluation2;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SecondActivity extends AppCompatActivity {
+    TextView tv;
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        tv= findViewById(R.id.textView);
+        Intent i = getIntent();
+
+        String n = i.getStringExtra("personName");
+        tv.setText("hello"+ n + "!");
+
+    }
+}
